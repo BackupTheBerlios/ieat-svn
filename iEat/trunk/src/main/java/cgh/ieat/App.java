17a5de1080495a3174eb9049496e788c83c24dc6
choward <choward@4c375a3e-0467-0410-8f3e-c6e20de2e7b3>
2009-04-08 19:45:35 +0000

    Cleaned up initial application and fixed splash screen and main app window.

--- iEat/trunk/src/main/java/cgh/ieat/App.java
+++ iEat/trunk/src/main/java/cgh/ieat/App.java
@@ -13,66 +13,94 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Shell;
 
-public class App {
-
-  public static void main(String[] args) {
-    final Display display = new Display();
-    final int[] count = new int[] { 4 };
-    final Image image = new Image(display, "iEat.gif");
-    final Shell splash = new Shell(SWT.ON_TOP);
-    final ProgressBar bar = new ProgressBar(splash, SWT.BOLD);
-    bar.setMaximum(count[0]);
-    Label label = new Label(splash, SWT.BOLD);
-    label.setImage(image);
-    FormLayout layout = new FormLayout();
-    splash.setLayout(layout);
-    FormData labelData = new FormData();
-    labelData.right = new FormAttachment(100, 0);
-    labelData.left = new FormAttachment(8, 0);
-    labelData.bottom = new FormAttachment(100, 0);
-    label.setLayoutData(labelData);
-    FormData progressData = new FormData();
-    progressData.left = new FormAttachment(0, 5);
-    progressData.right = new FormAttachment(100, -5);
-    progressData.bottom = new FormAttachment(100, -5);
-    bar.setLayoutData(progressData);
-    splash.pack();
+public class App
+{
+    final static int START_CNT = 4;
+    final static int SPLASH_WDTH = 100;
     
-    Rectangle splashRect = splash.getBounds();
-    Rectangle displayRect = display.getMonitors()[0].getBounds();
-    int x = (displayRect.width - splashRect.width) / 2;
-    int y = (displayRect.height - splashRect.height) / 2;
-    splash.setLocation(x, y);
-    splash.open();
-    display.asyncExec(new Runnable() {
-      public void run() {
-        Shell[] shells = new Shell[count[0]];
-        for (int i = 0; i < count[0]; i++) {
-          shells[i] = new Shell(display);
-          shells[i].setSize(600, 300);
-          shells[i].addListener(SWT.Close, new Listener() {
-            public void handleEvent(Event e) {
-              --count[0];
+    final static int APP_WDTH = 800;
+    final static int APP_HGTH = 600;
+
+    public static void main(String[] args)
+    {
+        final Display display = new Display();
+        final boolean[] appControl = new boolean[]
+            {
+                false
+            };
+        final Image image = new Image(display, "iEat.gif");
+        final Shell splash = new Shell(SWT.ON_TOP);
+        final ProgressBar bar = new ProgressBar(splash, SWT.BOLD);
+        
+        // Configure the splash screen
+        bar.setMaximum(START_CNT);
+        Label label = new Label(splash, SWT.BOLD);
+        label.setImage(image);
+        FormLayout layout = new FormLayout();
+        splash.setLayout(layout);
+        FormData labelData = new FormData();
+        labelData.right = new FormAttachment(SPLASH_WDTH, 0);
+        labelData.left = new FormAttachment(8, 0);
+        labelData.bottom = new FormAttachment(SPLASH_WDTH, 0);
+        label.setLayoutData(labelData);
+        FormData progressData = new FormData();
+        progressData.left = new FormAttachment(0, 5);
+        progressData.right = new FormAttachment(SPLASH_WDTH, -5);
+        progressData.bottom = new FormAttachment(SPLASH_WDTH, -5);
+        bar.setLayoutData(progressData);
+        splash.pack();
+
+        // Determines some screen placement info
+        Rectangle splashRect = splash.getBounds();
+        Rectangle displayRect = display.getMonitors()[0].getBounds();
+        final int x = (displayRect.width - splashRect.width) / 2;
+        final int y = (displayRect.height - splashRect.height) / 2;
+        
+        // Open the splash and start app
+        splash.setLocation(x, y);
+        splash.open();
+        display.asyncExec(new Runnable()
+        {
+            public void run()
+            {
+                // Start up open application
+                Shell shell;
+                shell = new Shell(display);
+                shell.setSize(APP_WDTH, APP_HGTH);
+                shell.setLocation(x - ((APP_WDTH/2) - SPLASH_WDTH), y - ((APP_HGTH/2) - SPLASH_WDTH));
+                shell.addListener(SWT.Close, new Listener()
+                {
+                    public void handleEvent(Event e)
+                    {
+                        appControl[0] = true;
+                    }
+                });
+                shell.open();
+                
+                // Delay startup and show progress bar/splash
+                for (int i = 0; i <= START_CNT; i++)
+                {
+                    bar.setSelection(i);
+                    try
+                    {
+                        Thread.sleep(1000);
+                    }
+                    catch (Throwable e)
+                    {
+                    }
+                }
+
+                // Close splash and cleanup
+                splash.close();
+                image.dispose();
             }
-          });
-          bar.setSelection(i + 1);
-          try {
-            Thread.sleep(1000);
-          } catch (Throwable e) {
-          }
+        });
+        while (!appControl[0])
+        {
+            if (!display.readAndDispatch())
+                display.sleep();
         }
-        splash.close();
-        image.dispose();
-        for (int i = 0; i < count[0]; i++) {
-          shells[i].open();
-        }
-      }
-    });
-    while (count[0] != 0) {
-      if (!display.readAndDispatch())
-        display.sleep();
+        display.dispose();
     }
-    display.dispose();
-  }
 
 }
